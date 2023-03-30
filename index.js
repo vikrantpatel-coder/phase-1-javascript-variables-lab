6948b8011058b55b8beeba13d11ef14682f529ab
vikrantpatel-coder <126751705+vikrantpatel-coder@users.noreply.github.com>
2023-03-29 21:50:41 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file!
+let companyName = 'Scuber';{
+let mostProfitableNeighborhood = 'Chelsea'
+let companyCeo = 'Susan Smith'
+
+    const companyName = Scuber
+
+    
+
+}
+    
+
+
+
+
